a194f4c35b5df3c3fea983457bb53fef2d33c98f
BuildTools <teaqllabs@gmail.com>
2022-01-26 00:19:29 -0500

    Update vmm.h

---
+++ src/x86_64/kernel/memory/vmm.h
@@ -0,0 +1,23 @@
+#ifndef VMM_H
+#define VMM_H
+
+#define IDENT_OFFSET 0x1 << 46
+#define PAGE_PRESENT 1 << 0
+#define PAGE_RW 1 << 1
+#define PAGE_USER 1 << 2
+#define PAGE_WRITETHROUGH 1 << 3
+#define PAGE_CACHE_DISABLE 1 << 4
+#define PAGE_ACCESSED 1 << 5
+#define PAGE_DIRTY 1 << 6
+#define PAGE_SIZE 1 << 7
+#define PAGE_GLOBAL 1 << 8
+#define PAGE_SOFTWARE 1 << 9
+#define PAGE_NX 1 << 63
+
+
+
+void vmm_init();
+uint64_t create_address_space();
+
+
+#endif
